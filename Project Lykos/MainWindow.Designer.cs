6c515b0d4166060ea1fd838989ce27cf2f7bf847
ionite <ionite@pm.me>
2022-04-03 20:53:37 -0400

    UI Updates

--- Project Lykos/MainWindow.Designer.cs
+++ Project Lykos/MainWindow.Designer.cs
@@ -37,7 +37,9 @@
             this.listView1 = new System.Windows.Forms.ListView();
             this.progress_total = new System.Windows.Forms.ProgressBar();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            this.comboBox_multiprocess_count = new System.Windows.Forms.ComboBox();
+            this.combo_csvDelimiter = new System.Windows.Forms.ComboBox();
+            this.label_csvDelimiter = new System.Windows.Forms.Label();
+            this.combo_multiprocess_count = new System.Windows.Forms.ComboBox();
             this.label_multiprocess_count = new System.Windows.Forms.Label();
             this.combo_audio_preprocessing = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -54,6 +56,7 @@
             this.button_browse_source = new System.Windows.Forms.Button();
             this.Group_ProcessingView = new System.Windows.Forms.GroupBox();
             this.Group_TotalProgress = new System.Windows.Forms.GroupBox();
+            this.label_lineCount = new System.Windows.Forms.Label();
             this.Group_Settings = new System.Windows.Forms.GroupBox();
             this.LayoutPanel_LowerButtons.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
@@ -70,7 +73,7 @@
             this.button_preview.Font = new System.Drawing.Font("Segoe UI Variable Display", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button_preview.Location = new System.Drawing.Point(13, 13);
             this.button_preview.Name = "button_preview";
-            this.button_preview.Size = new System.Drawing.Size(509, 49);
+            this.button_preview.Size = new System.Drawing.Size(506, 49);
             this.button_preview.TabIndex = 0;
             this.button_preview.Text = "Preview Indexed Operations";
             this.button_preview.UseVisualStyleBackColor = true;
@@ -85,22 +88,22 @@
             this.LayoutPanel_LowerButtons.Controls.Add(this.button3, 0, 1);
             this.LayoutPanel_LowerButtons.Controls.Add(this.button_stop_batch, 1, 1);
             this.LayoutPanel_LowerButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.LayoutPanel_LowerButtons.Location = new System.Drawing.Point(0, 540);
+            this.LayoutPanel_LowerButtons.Location = new System.Drawing.Point(0, 655);
             this.LayoutPanel_LowerButtons.Name = "LayoutPanel_LowerButtons";
             this.LayoutPanel_LowerButtons.Padding = new System.Windows.Forms.Padding(10, 10, 10, 24);
             this.LayoutPanel_LowerButtons.RowCount = 2;
             this.LayoutPanel_LowerButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.LayoutPanel_LowerButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.LayoutPanel_LowerButtons.Size = new System.Drawing.Size(1050, 144);
+            this.LayoutPanel_LowerButtons.Size = new System.Drawing.Size(1044, 144);
             this.LayoutPanel_LowerButtons.TabIndex = 1;
             // 
             // button_start_batch
             // 
             this.button_start_batch.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button_start_batch.Font = new System.Drawing.Font("Segoe UI Variable Display", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button_start_batch.Location = new System.Drawing.Point(528, 13);
+            this.button_start_batch.Location = new System.Drawing.Point(525, 13);
             this.button_start_batch.Name = "button_start_batch";
-            this.button_start_batch.Size = new System.Drawing.Size(509, 49);
+            this.button_start_batch.Size = new System.Drawing.Size(506, 49);
             this.button_start_batch.TabIndex = 1;
             this.button_start_batch.Text = "Start Batch";
             this.button_start_batch.UseVisualStyleBackColor = true;
@@ -111,7 +114,7 @@
             this.button3.Font = new System.Drawing.Font("Segoe UI Variable Display", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button3.Location = new System.Drawing.Point(13, 68);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(509, 49);
+            this.button3.Size = new System.Drawing.Size(506, 49);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
@@ -120,9 +123,9 @@
             // 
             this.button_stop_batch.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button_stop_batch.Font = new System.Drawing.Font("Segoe UI Variable Display", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button_stop_batch.Location = new System.Drawing.Point(528, 68);
+            this.button_stop_batch.Location = new System.Drawing.Point(525, 68);
             this.button_stop_batch.Name = "button_stop_batch";
-            this.button_stop_batch.Size = new System.Drawing.Size(509, 49);
+            this.button_stop_batch.Size = new System.Drawing.Size(506, 49);
             this.button_stop_batch.TabIndex = 3;
             this.button_stop_batch.Text = "Stop Batch";
             this.button_stop_batch.UseVisualStyleBackColor = true;
@@ -134,7 +137,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.listView1.Location = new System.Drawing.Point(6, 32);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(497, 480);
+            this.listView1.Size = new System.Drawing.Size(491, 595);
             this.listView1.TabIndex = 2;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
@@ -150,31 +153,65 @@
             this.tableLayoutPanel3.ColumnCount = 2;
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel3.Controls.Add(this.comboBox_multiprocess_count, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.combo_csvDelimiter, 1, 2);
+            this.tableLayoutPanel3.Controls.Add(this.label_csvDelimiter, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.combo_multiprocess_count, 1, 1);
             this.tableLayoutPanel3.Controls.Add(this.label_multiprocess_count, 0, 1);
             this.tableLayoutPanel3.Controls.Add(this.combo_audio_preprocessing, 1, 0);
             this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel3.Location = new System.Drawing.Point(9, 29);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
-            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowCount = 3;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel3.Size = new System.Drawing.Size(476, 193);
             this.tableLayoutPanel3.TabIndex = 8;
             // 
-            // comboBox_multiprocess_count
+            // combo_csvDelimiter
             // 
-            this.comboBox_multiprocess_count.Dock = System.Windows.Forms.DockStyle.Top;
-            this.comboBox_multiprocess_count.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox_multiprocess_count.FormattingEnabled = true;
-            this.comboBox_multiprocess_count.Items.AddRange(new object[] {
-            "None (Not Recommended)",
-            "Resampled (Standard)",
-            "Smoothed (Safest)"});
-            this.comboBox_multiprocess_count.Location = new System.Drawing.Point(188, 41);
-            this.comboBox_multiprocess_count.Name = "comboBox_multiprocess_count";
-            this.comboBox_multiprocess_count.Size = new System.Drawing.Size(285, 32);
-            this.comboBox_multiprocess_count.TabIndex = 3;
+            this.combo_csvDelimiter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.combo_csvDelimiter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.combo_csvDelimiter.FormattingEnabled = true;
+            this.combo_csvDelimiter.Items.AddRange(new object[] {
+            "Auto Detect",
+            "Comma [ , ] ",
+            "Semicolon [ ; ]",
+            "Vertical Slash [ | ]"});
+            this.combo_csvDelimiter.Location = new System.Drawing.Point(188, 79);
+            this.combo_csvDelimiter.Name = "combo_csvDelimiter";
+            this.combo_csvDelimiter.Size = new System.Drawing.Size(285, 32);
+            this.combo_csvDelimiter.TabIndex = 5;
+            // 
+            // label_csvDelimiter
+            // 
+            this.label_csvDelimiter.AutoSize = true;
+            this.label_csvDelimiter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label_csvDelimiter.Location = new System.Drawing.Point(3, 76);
+            this.label_csvDelimiter.Name = "label_csvDelimiter";
+            this.label_csvDelimiter.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.label_csvDelimiter.Size = new System.Drawing.Size(179, 29);
+            this.label_csvDelimiter.TabIndex = 4;
+            this.label_csvDelimiter.Text = "CSV Delimiter";
+            // 
+            // combo_multiprocess_count
+            // 
+            this.combo_multiprocess_count.Dock = System.Windows.Forms.DockStyle.Top;
+            this.combo_multiprocess_count.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.combo_multiprocess_count.FormattingEnabled = true;
+            this.combo_multiprocess_count.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.combo_multiprocess_count.Location = new System.Drawing.Point(188, 41);
+            this.combo_multiprocess_count.Name = "combo_multiprocess_count";
+            this.combo_multiprocess_count.Size = new System.Drawing.Size(285, 32);
+            this.combo_multiprocess_count.TabIndex = 3;
             // 
             // label_multiprocess_count
             // 
@@ -185,7 +222,7 @@
             this.label_multiprocess_count.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
             this.label_multiprocess_count.Size = new System.Drawing.Size(179, 29);
             this.label_multiprocess_count.TabIndex = 2;
-            this.label_multiprocess_count.Text = "Multithreading:";
+            this.label_multiprocess_count.Text = "Multiprocess Count:";
             // 
             // combo_audio_preprocessing
             // 
@@ -193,9 +230,10 @@
             this.combo_audio_preprocessing.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.combo_audio_preprocessing.FormattingEnabled = true;
             this.combo_audio_preprocessing.Items.AddRange(new object[] {
-            "None (Not Recommended)",
+            "None (Requires 16kHz 16bit mono)",
+            "FaceFX Native",
             "Resampled (Standard)",
-            "Smoothed (Safest)"});
+            "Smooth Resample (Safest)"});
             this.combo_audio_preprocessing.Location = new System.Drawing.Point(188, 3);
             this.combo_audio_preprocessing.Name = "combo_audio_preprocessing";
             this.combo_audio_preprocessing.Size = new System.Drawing.Size(285, 32);
@@ -277,6 +315,8 @@
             this.combo_csv.Name = "combo_csv";
             this.combo_csv.Size = new System.Drawing.Size(336, 32);
             this.combo_csv.TabIndex = 5;
+            this.combo_csv.Enter += new System.EventHandler(this.Combo_Any_Enter_Leave);
+            this.combo_csv.Leave += new System.EventHandler(this.Combo_Any_Enter_Leave);
             // 
             // label3
             // 
@@ -297,6 +337,8 @@
             this.combo_output.Name = "combo_output";
             this.combo_output.Size = new System.Drawing.Size(336, 32);
             this.combo_output.TabIndex = 3;
+            this.combo_output.Enter += new System.EventHandler(this.Combo_Any_Enter_Leave);
+            this.combo_output.Leave += new System.EventHandler(this.Combo_Any_Enter_Leave);
             // 
             // label2
             // 
@@ -317,6 +359,8 @@
             this.combo_source.Name = "combo_source";
             this.combo_source.Size = new System.Drawing.Size(336, 32);
             this.combo_source.TabIndex = 0;
+            this.combo_source.Enter += new System.EventHandler(this.Combo_Any_Enter_Leave);
+            this.combo_source.Leave += new System.EventHandler(this.Combo_Any_Enter_Leave);
             // 
             // label_source
             // 
@@ -342,24 +386,39 @@
             // 
             // Group_ProcessingView
             // 
+            this.Group_ProcessingView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.Group_ProcessingView.Controls.Add(this.listView1);
             this.Group_ProcessingView.Font = new System.Drawing.Font("Segoe UI Variable Display", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Group_ProcessingView.Location = new System.Drawing.Point(528, 17);
             this.Group_ProcessingView.Name = "Group_ProcessingView";
-            this.Group_ProcessingView.Size = new System.Drawing.Size(509, 517);
+            this.Group_ProcessingView.Size = new System.Drawing.Size(503, 632);
             this.Group_ProcessingView.TabIndex = 10;
             this.Group_ProcessingView.TabStop = false;
             this.Group_ProcessingView.Text = "Processing View";
             // 
             // Group_TotalProgress
             // 
+            this.Group_TotalProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Group_TotalProgress.Controls.Add(this.label_lineCount);
             this.Group_TotalProgress.Controls.Add(this.progress_total);
             this.Group_TotalProgress.Location = new System.Drawing.Point(13, 445);
             this.Group_TotalProgress.Name = "Group_TotalProgress";
-            this.Group_TotalProgress.Size = new System.Drawing.Size(500, 90);
+            this.Group_TotalProgress.Size = new System.Drawing.Size(501, 204);
             this.Group_TotalProgress.TabIndex = 11;
             this.Group_TotalProgress.TabStop = false;
-            this.Group_TotalProgress.Text = "Total Progress";
+            this.Group_TotalProgress.Text = "Progress";
+            // 
+            // label_lineCount
+            // 
+            this.label_lineCount.AutoSize = true;
+            this.label_lineCount.Location = new System.Drawing.Point(16, 68);
+            this.label_lineCount.Name = "label_lineCount";
+            this.label_lineCount.Size = new System.Drawing.Size(57, 24);
+            this.label_lineCount.TabIndex = 4;
+            this.label_lineCount.Text = "label4";
             // 
             // Group_Settings
             // 
@@ -376,7 +435,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1050, 684);
+            this.ClientSize = new System.Drawing.Size(1044, 799);
             this.Controls.Add(this.Group_Settings);
             this.Controls.Add(this.Group_TotalProgress);
             this.Controls.Add(this.Group_ProcessingView);
@@ -385,6 +444,7 @@
             this.Font = new System.Drawing.Font("Segoe UI Variable Display", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Project Lykos - Lip Generation Tool";
             this.Load += new System.EventHandler(this.MainWindow_Load);
             this.LayoutPanel_LowerButtons.ResumeLayout(false);
@@ -395,6 +455,7 @@
             this.tableLayoutPanel2.PerformLayout();
             this.Group_ProcessingView.ResumeLayout(false);
             this.Group_TotalProgress.ResumeLayout(false);
+            this.Group_TotalProgress.PerformLayout();
             this.Group_Settings.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -416,7 +477,7 @@
         private GroupBox Group_Settings;
         private ComboBox combo_audio_preprocessing;
         private Label label1;
-        private ComboBox comboBox_multiprocess_count;
+        private ComboBox combo_multiprocess_count;
         private Label label_multiprocess_count;
         private TableLayoutPanel tableLayoutPanel2;
         private ComboBox combo_output;
@@ -428,5 +489,8 @@
         private Button button_browse_csv;
         private Button button_browse_output;
         private Button button_browse_source;
+        private Label label_lineCount;
+        private ComboBox combo_csvDelimiter;
+        private Label label_csvDelimiter;
     }
 }
