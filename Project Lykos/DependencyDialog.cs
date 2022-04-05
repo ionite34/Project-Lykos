3746154fbcc0e9ada328625dd2c7a62751d4cdab
ionite <ionite@pm.me>
2022-04-04 20:54:12 -0400

    Updated Dialog with backend links for buttons.

--- Project Lykos/DependencyDialog.cs
+++ Project Lykos/DependencyDialog.cs
@@ -15,13 +15,37 @@ namespace Project_Lykos
         public DependencyDialog()
         {
             InitializeComponent();
-            button_exit.DialogResult = DialogResult.Cancel;
-            button_retry.DialogResult = DialogResult.Retry;
         }
 
         private void DependencyDialog_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void LinkLabel_download_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string target = "https://www.nexusmods.com/skyrimspecialedition/mods/40971?tab=files";
+            System.Diagnostics.Process.Start(target);
+        }
+
+        private void Button_exit_Click(object sender, EventArgs e)
+        {
+            // Close the entire application
+            button_exit.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void Button_retry_Click(object sender, EventArgs e)
+        {
+            // Recheck dependency
+            if (DependencyCheck.CheckFonixData()) {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Unable to locate FonixData.cdf, please ensure it is in the same folder as this application.", "FonixData.cdf Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
