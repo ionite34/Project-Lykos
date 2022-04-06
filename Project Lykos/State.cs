a740cce11816100fefcc04ce79e32d1f53d13ef3
ionite <ionite@pm.me>
2022-04-06 12:34:38 -0400

    Added State tracking offloading for main window

---
+++ Project Lykos/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Lykos
+{
+    // Holds and computes state of ready to run operations
+    internal class State
+    {
+        // States
+        public bool ReadyPreview { get; } = false;
+
+        // Constructor
+        
+    }
+}
