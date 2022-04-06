2fcdcd79b150fb94b2e83fc3e2695951b405c9be
ionite <ionite@pm.me>
2022-04-06 01:09:07 -0400

    Moved class scope

---
+++ Project Lykos/CSVDelimiterDetector.cs
@@ -0,0 +1,25 @@
+﻿namespace Project_Lykos;
+
+internal static class CSVDelimiterDetector
+{
+    private static readonly char[] SeparatorChars = { ';', '|', '\t', ',' };
+
+    public static char DetectSeparator(string csvFilePath)
+    {
+        var lines = File.ReadAllLines(csvFilePath);
+        var linesList = lines.ToList();
+        return DetectSeparator(linesList);
+    }
+
+    public static char DetectSeparator(List<string> lines)
+    {
+        var q = SeparatorChars.Select(sep => new
+                { Separator = sep, Found = lines.GroupBy(line => line.Count(ch => ch == sep)) })
+            .OrderByDescending(res => res.Found.Count(grp => grp.Key > 0))
+            .ThenBy(res => res.Found.Count())
+            .First();
+
+        // Default behavior returns '\0' if no separator was found
+        return q.Separator;
+    }
+}
