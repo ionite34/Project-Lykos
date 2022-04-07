8ef0882d95aa5b91a0f8b787c010ad247f9e8835
ionite <ionite@pm.me>
2022-04-06 23:44:14 -0400

    Added new progress link for integer

--- Project Lykos/ElementLink.cs
+++ Project Lykos/ElementLink.cs
@@ -14,6 +14,7 @@ namespace Project_Lykos
     /// </summary>
     public static class ElementLink
     {
+
         /// <summary>
         /// Returns an IProgress object that updates provided UI elements
         /// </summary>
@@ -23,6 +24,7 @@ namespace Project_Lykos
         /// <param name="usePercent"></param>
         /// <param name="displayTotal"></param>
         /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
         /// <exception cref="IndexOutOfRangeException"></exception>
         public static IProgress<(double current, double total)> LinkProgressBar2L(ProgressBar progressbar, Label value, Label status, bool usePercent = false, bool displayTotal = false)
         {
@@ -104,6 +106,111 @@ namespace Project_Lykos
                     }
                 }
                 
+                // Initialize the labels as visible.
+                if (!(value.Visible))
+                {
+                    value.BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        value.Visible = true;
+
+                    });
+                }
+                if (!(status.Visible))
+                {
+                    value.BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        status.Visible = true;
+
+                    });
+                }
+            });
+            return progress;
+        }
+
+
+        /// <summary>
+        /// Returns an IProgress object that updates provided UI elements
+        /// </summary>
+        /// <param name="progressbar"></param>
+        /// <param name="value"></param>
+        /// <param name="status"></param>
+        /// <param name="usePercent"></param>
+        /// <param name="displayTotal"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        public static IProgress<(int current, int total)> LinkProgressBarInt2L(ProgressBar progressbar, Label value, Label status, bool usePercent = false, bool displayTotal = false)
+        {
+            var progress = new Progress<(int current, int total)>(update =>
+            {
+                if (usePercent && displayTotal)
+                {
+                    throw new InvalidOperationException("Cannot use both percent and total");
+                }
+
+                var (current, total) = update;
+
+                if (total == 0 || total < current)
+                {
+                    throw new IndexOutOfRangeException("Progress update provided invalid values.");
+                }
+                
+                var newValue = current;
+                var totalValue = total;
+                var curPercent = progressbar.Value / total;
+
+                if (progressbar.Maximum != totalValue)
+                {
+                    progressbar.BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        progressbar.Maximum = totalValue;
+                    });
+                }
+
+                if (progressbar.Value != newValue)
+                {
+                    progressbar.BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        progressbar.Value = newValue;
+                    });
+                }
+
+                if (usePercent) // Mode '0.00%'
+                {
+                    var p = curPercent.ToString("0%"); // Format percent
+                    if (value.Text != p)
+                    {
+                        value.BeginInvoke((MethodInvoker)delegate ()
+                        {
+                            value.Text = p;
+                        });
+                    }
+                }
+                else if (displayTotal) // Mode '0,000/0,000'
+                {
+                    var c = newValue.ToString("N0"); // Format commas
+                    var t = totalValue.ToString("N0"); // Format commas
+                    var str = $"{c}/{t}";
+                    if (value.Text != str)
+                    {
+                        value.BeginInvoke((MethodInvoker)delegate ()
+                        {
+                            value.Text = str;
+                        });
+                    }
+                }
+                else // Mode: '0,000'
+                {
+                    var c = newValue.ToString("N0"); // Format commas
+                    if (value.Text != c)
+                    {
+                        value.BeginInvoke((MethodInvoker)delegate ()
+                        {
+                            value.Text = c;
+                        });
+                    }
+                }
+
                 // Initialize the labels as visible.
                 if (!(value.Visible))
                 {
