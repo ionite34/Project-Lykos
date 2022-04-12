5b780aa47eb08e7311c82e49476cea29762ae430
ionite <ionite@pm.me>
2022-04-11 22:28:46 -0400

    Added new deployment for FX Extended

--- Project Lykos/Cache.cs
+++ Project Lykos/Cache.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,42 +14,71 @@ namespace Project_Lykos
         public static readonly string TempDir = Path.GetTempPath();
         public static string FullTempDir => Path.Join(TempDir, "Lykos_Temp");
         public static string WrapDir => Path.Join(FullTempDir, "Wrapper");
-        public static string WrapPath => Path.Join(WrapDir, "FaceFXWrapper.exe");
+        public static string DataPath => Path.Join(FullTempDir, "Wrapper", "FonixData.cdf");
+        public static string WrapPath => Path.Join(WrapDir, "FaceFXWrapperExtended.exe");
         public static string AudioDir => Path.Join(FullTempDir, "AudioSource");
-        public static string AudioResamplingDir => Path.Join(FullTempDir, "ResampleCache");
+        public static string LogDir => Path.Join(Directory.GetCurrentDirectory(), "Logs");
 
-        // Creates the temp directory
-        public static void Create()
+        // Storage of args
+        public static string ArgType { get; } = "Skyrim";
+        public static string ArgLang { get; } = "USEnglish";
+        
+        // Storage of processing time
+        public static List<double> ProcessingTimes { set; get; } = new List<double>();
+        // Returns the average of the processing times
+        public static double AverageProcessingTime => ProcessingTimes.Average();
+        
+        // Running
+        public static int ProcessesRunning { set; get; } = 0;
+        
+        // Setups the temp directory for use
+        public static void Setup()
         {
-            if (Directory.Exists(FullTempDir)) return;
-            Directory.CreateDirectory(FullTempDir);
-            Directory.CreateDirectory(Path.Join(FullTempDir, "Wrapper"));
-            Directory.CreateDirectory(Path.Join(FullTempDir, "Source_Audio"));
+            Destroy();
+            Create();
+            DeployFaceFX();
+            DeployFonixData();
         }
-
+        
         // Deletes the temp directory
         public static void Destroy()
         {
             if (!Directory.Exists(FullTempDir)) return;
             Directory.Delete(FullTempDir, true);
         }
-
-        public static void DeployFaceFX(string relativeTempPath = "Wrapper")
+        
+        // Creates the temp directory
+        private static void Create()
+        {
+            if (Directory.Exists(FullTempDir)) return;
+            Directory.CreateDirectory(FullTempDir);
+            Directory.CreateDirectory(WrapDir);
+            Directory.CreateDirectory(AudioDir);
+        }
+        
+         // Deploys the FaceFXWrapper.exe to the temp directory
+        private static void DeployFaceFX(string relativeTempPath = "Wrapper")
         {
             Create();
-            var wrapDir = Path.Join(FullTempDir, relativeTempPath);
-            File.WriteAllBytes(wrapDir, Properties.Resources.FaceFXWrapper);
+            File.WriteAllBytes(WrapPath, Properties.Resources.FaceFXWrapperExtended);
         }
 
-        public static void DeployFonixData(string relativeTempPath = "Wrapper")
+        private static void DeployFonixData(string relativeTempPath = "Wrapper")
         {
-            if (!(DependencyCheck.CheckFonixData())) throw new IOException("Fonix data not found");
+            if (!(DependencyCheck.CheckFonixData())) throw new IOException(@"Fonix data not found");
             Create();
             var appDirectory = Directory.GetCurrentDirectory();
             var path = Path.Combine(appDirectory, "FonixData.cdf");
             // Move the file from path to the temp directory
-            File.Copy(path, Path.Join(FullTempDir, relativeTempPath, "FonixData.cdf"));
+            File.Copy(path, Path.Join(FullTempDir, relativeTempPath, @"FonixData.cdf"));
         }
 
+        public static IEnumerable<T> DequeueChunk<T>(this Queue<T> queue, int chunkSize) 
+        {
+            for (int i = 0; i < chunkSize && queue.Count > 0; i++)
+            {
+                yield return queue.Dequeue();
+            }
+        }
     }
 }
