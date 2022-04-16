bcaa5fbb957f27c41eed31d47a823499a0389267
Ionite <ionite@pm.me>
2022-04-16 11:55:32 -0400

    Added time estimate class

---
+++ Project Lykos/TimeEstimate.cs
@@ -0,0 +1,42 @@
+namespace Project_Lykos;
+
+public class TimeEstimate
+{
+    // Start time of the estimate
+    private readonly DateTime startTime;
+    // Upper index of process batch (not count)
+    private readonly int indexMax;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="maxCount">amount of items for completion</param>
+    public TimeEstimate(int maxCount)
+    {
+        indexMax = maxCount + 1;
+        startTime = DateTime.Now;
+    }
+
+    /// <summary>
+    /// Returns the estimated time of completion
+    /// </summary>
+    /// <param name="iterationsDone"></param>
+    /// <returns>Estimated time of completion as DateTime</returns>
+    public DateTime GetEtc(int iterationsDone)
+    {
+        var iterationsTotal = indexMax + 1;
+        var msElapsed = DateTime.Now.Subtract(startTime).TotalMilliseconds;
+        var unitTime = msElapsed / (double)iterationsDone;
+        var etc = DateTime.Now.AddMilliseconds(unitTime * (iterationsTotal - iterationsDone));
+        return etc;
+    }
+    
+    public double GetItemsPerSecond(int iterationsDone)
+    {
+        var iterationsTotal = indexMax + 1;
+        var msElapsed = DateTime.Now.Subtract(startTime).TotalMilliseconds;
+        var unitTime = msElapsed / (double)iterationsDone; // ms per item
+        var itemsPerSecond = 1000 / unitTime;
+        return itemsPerSecond;
+    }
+}
