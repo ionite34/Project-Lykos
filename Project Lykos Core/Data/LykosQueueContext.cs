dd084e385acef8e6f4e79325f439cc9384fb23e1
Chris Schaller <Chris@timeoutit.com>
2022-04-20 19:39:24 +1000

    Fixed issue with connection strings in the WinForms Master app... Previous testing used a hardcoded connection string, app.config has been implemented so that the winforms runtim can do it's thing

--- Project Lykos Core/Data/LykosQueueContext.cs
+++ Project Lykos Core/Data/LykosQueueContext.cs
@@ -21,7 +21,12 @@ namespace Project_Lykos.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("name=LykosQueue");
+            //optionsBuilder.UseSqlServer("name=LykosQueue");
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["LykosQueue"]?.ConnectionString;
+            if (string.IsNullOrWhiteSpace(connectionString))
+                optionsBuilder.UseSqlServer("name=LykosQueue");
+            else
+                optionsBuilder.UseSqlServer(connectionString);
         }
         public DbSet<Data.QueueEntry> QueueEntries { get; set; }
         public DbSet<Data.LogEntry> LogEntries { get; set; }
