56b89f4096e584109a4157673f279ca5fefc72ab
Ionite <ionite@pm.me>
2022-04-20 17:23:10 -0400

    Added WordParser

---
+++ Project Lykos/Word Checker/WordParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Lykos.Word_Checker
+{
+    internal class WordParser
+    {
+        // Paths
+        public DynamicPath CSVPath { get; set; } = new();
+        public DynamicPath DictPath { get; set; } = new();
+
+        // Returns true if ready to start batch
+        public bool ReadyRefresh()
+        {
+            return CSVPath.Exists() && DictPath.Exists();
+        }
+
+        public void SetFilepath_CSV(string filepath)
+        {
+            if (!System.IO.Directory.Exists(filepath)) throw new Exception("Directory not found.");
+            CSVPath.SetPath(filepath);
+            Task.Run(() =>
+            {
+                // Indexer.BackgroundIndexFiles(filepath);
+                // ProcessWorker.FilesTable = Indexer.IndexData;
+            });
+        }
+
+        public void SetFilepath_Dict(string filepath)
+        {
+            if (!System.IO.Directory.Exists(filepath)) throw new Exception("Directory not found.");
+            DictPath.SetPath(filepath);
+        }
+
+    }
+}
